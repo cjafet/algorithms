4938655356f1057a80f71c2bd936a23c66702691
Carlos Jafet Neto <cjafet07@gmail.com>
2023-08-27 14:08:51 -0700

    Selection Sort

---
+++ src/main/java/SelectionSort.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class SelectionSort {
+
+    private static List<Integer> list = Arrays.asList(64, 25, 12, 22, 11);
+
+    public static void init() {
+        showHeader();
+        for (int j = 0; j < list.size(); j++) {
+            List<Integer> tmpList = new ArrayList<>();
+            for (int k = j; k < list.size(); k++) {
+                tmpList.add(list.get(k));
+            }
+            int min = Collections.min(tmpList);
+            int pos = list.indexOf(min);
+            swap(pos, j);
+        }
+    }
+
+    private static void swap(int pos, int i) {
+        if(!list.get(i).equals(list.get(pos))) {
+            int swap = list.get(i);
+            list.set(i,list.get(pos));
+            list.set(pos, swap);
+            System.out.println(list);
+        }
+    }
+
+    private static void showHeader() {
+        System.out.println("Initial Array");
+        System.out.println("--------------------");
+        System.out.println(list);
+        System.out.println("--------------------");
+    }
+}
